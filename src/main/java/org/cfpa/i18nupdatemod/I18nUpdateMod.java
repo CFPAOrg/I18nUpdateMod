41183edcff4b17ee4f15b5a5aebe69890976b326
TartaricAcid <baka943@qq.com>
2018-05-19 15:37:15 +0800

    完成 options.txt 修改问题

--- src/main/java/org/cfpa/i18nupdatemod/I18nUpdateMod.java
+++ src/main/java/org/cfpa/i18nupdatemod/I18nUpdateMod.java
@@ -1,12 +1,18 @@
 package org.cfpa.i18nupdatemod;
 
+import com.google.common.collect.Lists;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.ResourcePackRepository;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.cfpa.i18nupdatemod.download.MainDownloader;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
 
 
 @Mod(modid = I18nUpdateMod.MODID, name = I18nUpdateMod.NAME, clientSideOnly = true, acceptedMinecraftVersions = "[1.12]", version = I18nUpdateMod.VERSION)
@@ -32,7 +38,7 @@ public class I18nUpdateMod {
         //记录开始时间
         long startTime = System.currentTimeMillis();
         try {
-            MainDownloader.downloadResource("https://media.forgecdn.net/files/2557/405/Minecraft-Mod-Language-Modpack.zip", "Minecraft-Mod-Language-Modpack.zip", mc.getResourcePackRepository().getDirResourcepacks().toString());
+            MainDownloader.downloadResource("https://github.com/CFPAOrg/Minecraft-Mod-Language-Package/releases/download/%E6%B1%89%E5%8C%96%E8%B5%84%E6%BA%90%E5%8C%85-Snapshot-20180518064901/Minecraft-Mod-Language-Modpack.zip", "Minecraft-Mod-Language-Modpack.zip", mc.getResourcePackRepository().getDirResourcepacks().toString());
             logger.info("下载成功！");
         } catch (Exception e) {
             logger.error("下载失败！");
@@ -41,18 +47,62 @@ public class I18nUpdateMod {
         //记录结束时间
         long endTime = System.currentTimeMillis();
         float excTime = (float) (endTime - startTime) / 1000;
-        logger.info("花费时间：" + excTime);
+        logger.info("花费时间：" + excTime + "秒");
     }
 
     public void applyOption() {
+        // 应用修改
+        createOptionFile();
+
         Minecraft mc = Minecraft.getMinecraft();
-        GameSettings setting = mc.gameSettings;
+        GameSettings gameSettings = mc.gameSettings;
 
-        setting.loadOptions();
-        setting.language = "zh_cn";
-        setting.resourcePacks.add(0, "Minecraft-Mod-Language-Modpack.zip");
-        setting.saveOptions();
+        // 因为这时候资源包已经加载了，所以需要重新读取，重新加载
+        ResourcePackRepository resourcePackRepository = mc.getResourcePackRepository();
+        resourcePackRepository.updateRepositoryEntriesAll();
+        List<ResourcePackRepository.Entry> repositoryEntriesAll = resourcePackRepository.getRepositoryEntriesAll();
+        List<ResourcePackRepository.Entry> repositoryEntries = Lists.newArrayList();
+        Iterator<String> it = gameSettings.resourcePacks.iterator();
 
-        logger.info("成功加载！");
+        // 此时情况是这样的
+        // entry 为修改后的条目
+        // repositoryEntries 为游戏应当加载的条目
+        while (it.hasNext()) {
+            String packName = it.next();
+            for (ResourcePackRepository.Entry entry : repositoryEntriesAll) {
+                if (entry.getResourcePackName().equals(packName)) {
+                    // packFormat 为 3，或者 incompatibleResourcePacks 条目中有的资源包才会加入
+                    if (entry.getPackFormat() == 3 || gameSettings.incompatibleResourcePacks.contains(entry.getResourcePackName())) {
+                        repositoryEntries.add(entry);
+                        break;
+                    }
+                    // 否则移除
+                    it.remove();
+                    logger.warn("移除资源包 {}，因为它无法兼容当前版本", entry.getResourcePackName());
+                }
+            }
+        }
+        resourcePackRepository.setRepositories(repositoryEntries);
+    }
+
+    public void createOptionFile() {
+        Minecraft mc = Minecraft.getMinecraft();
+
+        File option = new File(mc.mcDataDir, "options.txt");
+        if (!option.exists()) {
+            try {
+                FileOutputStream optionFos = new FileOutputStream(option);
+                OutputStreamWriter writer = new OutputStreamWriter(optionFos);
+                writer.append("resourcePacks:[\"Minecraft-Mod-Language-Modpack.zip\"]");
+                writer.close();
+                optionFos.close();
+            } catch (IOException e) {
+                logger.error(e.getStackTrace().toString());
+                logger.error("无法创建配置文件");
+            }
+        }
+        if (option.exists() && !mc.gameSettings.resourcePacks.contains("Minecraft-Mod-Language-Modpack.zip")) {
+            mc.gameSettings.resourcePacks.add("Minecraft-Mod-Language-Modpack.zip");
+        }
     }
 }
