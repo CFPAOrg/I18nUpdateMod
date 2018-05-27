9361d2a25bce9549dae949234df66c9e1d9c4e8a
TartaricAcid <baka943@qq.com>
2018-05-27 17:18:12 +0800

    现在JEI右侧面板也可以通过快捷键反馈了

--- src/main/java/org/cfpa/i18nupdatemod/report/ReportKey.java
+++ src/main/java/org/cfpa/i18nupdatemod/report/ReportKey.java
@@ -1,5 +1,8 @@
 package org.cfpa.i18nupdatemod.report;
 
+import mezz.jei.Internal;
+import mezz.jei.gui.overlay.IngredientListOverlay;
+import mezz.jei.runtime.JeiRuntime;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -8,6 +11,7 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -34,28 +38,34 @@ public class ReportKey {
     @SideOnly(Side.CLIENT)
     public static void onKeyPress(GuiScreenEvent.KeyboardInputEvent.Pre e) {
         GuiScreen guiScreen = Minecraft.getMinecraft().currentScreen;
+
+        // 用于取消重复显示
         if (showed) {
             if (!Keyboard.isKeyDown(reportKey.getKeyCode())) {
                 showed = false;
             }
             return;
         }
+
+        // 当当前 GUI 为继承自原版 GuiContainer 的事物的时候
         if (guiScreen instanceof GuiContainer && Keyboard.getEventKey() == reportKey.getKeyCode()) {
             GuiContainer guiContainer = (GuiContainer) guiScreen;
             Slot slotUnderMouse = guiContainer.getSlotUnderMouse();
             if (slotUnderMouse != null) {
                 ItemStack stack = slotUnderMouse.getStack();
                 if (!stack.isEmpty()) {
-                    String text = String.format("模组ID：%s\n非本地化名称：%s\n显示名称：%s", stack.getItem().getCreatorModId(stack), stack.getItem().getUnlocalizedName(), stack.getDisplayName());
-                    String url = "https://wj.qq.com/s/2135580/0e03/";
-                    try {
-                        copyToClipboard(text);
-                        Desktop.getDesktop().browse(new URI(url));
-                    } catch (Exception urlException) {
-                        urlException.printStackTrace();
-                    }
-                    showed = true;
+                    showed = openBrowse(stack);
                 }
+            }
+        }
+
+        // 当前 GUI 为 JEI 时候
+        if (Loader.isModLoaded("jei") && Keyboard.getEventKey() == reportKey.getKeyCode()) {
+            JeiRuntime runtime = Internal.getRuntime();
+            IngredientListOverlay ingredientListOverlay = runtime.getItemListOverlay();
+            ItemStack stack = ingredientListOverlay.getStackUnderMouse();
+            if (stack != null) {
+                showed = openBrowse(stack);
             }
         }
     }
@@ -68,5 +78,18 @@ public class ReportKey {
         Transferable trans = new StringSelection(text);
         // 把文本内容设置到系统剪贴板
         clipboard.setContents(trans, null);
+    }
+
+    // 获取物品信息，并且打开浏览器
+    public static boolean openBrowse(ItemStack stack) {
+        String text = String.format("模组ID：%s\n非本地化名称：%s\n显示名称：%s", stack.getItem().getCreatorModId(stack), stack.getItem().getUnlocalizedName(), stack.getDisplayName());
+        String url = "https://wj.qq.com/s/2135580/0e03/";
+        try {
+            copyToClipboard(text);
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (Exception urlException) {
+            urlException.printStackTrace();
+        }
+        return true;
     }
 }
