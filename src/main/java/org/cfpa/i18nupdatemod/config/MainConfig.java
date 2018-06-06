180334183192845786b7282cfb531e07ce4e6b8f
Seraph_JACK <seraphjack@outlook.com>
2018-06-06 20:19:21 +0800

    配置文件修正

--- src/main/java/org/cfpa/i18nupdatemod/config/MainConfig.java
+++ src/main/java/org/cfpa/i18nupdatemod/config/MainConfig.java
@@ -28,7 +28,7 @@ public class MainConfig {
         public boolean showWeblateButton = true;
 
         @Config.Name("公告链接")
-        @Config.Comment("专为整合作者设计，你只需要提供一个纯网页版txt文件链接，即可加载此公告，注意是 http 网站")
+        @Config.Comment("专为整合作者设计，你只需要提供一个纯网页版txt文件链接，即可加载此公告")
         public String noticeURL = "http://downloader.meitangdehulu.com/Notice.txt";
     }
 
