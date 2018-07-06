57562a5c44bfe7bb74aeab9fc2fc1fbe04fed422
Seraph_JACK <seraphjack@outlook.com>
2018-07-06 14:09:21 +0800

    修正注释

--- src/main/java/org/cfpa/i18nupdatemod/report/HotKeyHandler.java
+++ src/main/java/org/cfpa/i18nupdatemod/report/HotKeyHandler.java
@@ -98,10 +98,6 @@ public class HotKeyHandler {
 
     /**
      * 获取物品信息，并打开浏览器
-<<<<<<< HEAD:src/main/java/org/cfpa/i18nupdatemod/report/HotKeyHandler.java
-     *
-=======
->>>>>>> a033edf... Cleanup round3:src/main/java/org/cfpa/i18nupdatemod/report/HotKeyHandler.java
      * @param stack 物品
      * @return 是否成功
      */
@@ -120,10 +116,6 @@ public class HotKeyHandler {
 
     /**
      * 获取物品信息，并打开 weblate 对应界面
-<<<<<<< HEAD:src/main/java/org/cfpa/i18nupdatemod/report/HotKeyHandler.java
-     *
-=======
->>>>>>> a033edf... Cleanup round3:src/main/java/org/cfpa/i18nupdatemod/report/HotKeyHandler.java
      * @param stack 物品
      * @return 是否成功
      */
@@ -152,10 +144,6 @@ public class HotKeyHandler {
 
     /**
      * 获取物品信息，并打开 mcmod 对应界面
-<<<<<<< HEAD:src/main/java/org/cfpa/i18nupdatemod/report/HotKeyHandler.java
-     *
-=======
->>>>>>> a033edf... Cleanup round3:src/main/java/org/cfpa/i18nupdatemod/report/HotKeyHandler.java
      * @param stack 物品
      * @return 是否成功
      */
